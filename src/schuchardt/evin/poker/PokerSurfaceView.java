449a2f12e393b8a85d32900cd0b9d64e415a4b3e
unknown <eschuchardt@evin.(none)>
2012-04-16 10:44:07 -0500

    MainActivity and SurfaceView just created.

---
+++ src/schuchardt/evin/poker/PokerSurfaceView.java
@@ -0,0 +1,32 @@
+package schuchardt.evin.poker;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class PokerSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
+	public PokerSurfaceView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
